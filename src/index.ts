dd9a888577c91ad5d437dbc30353011dce137071
Iulian Preda <iulian.octavian.preda@gmail.com>
2021-03-14 15:04:19 +0100

    Added comments

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,21 @@ const pathToInput = path.format({
     base: 'input.txt'
 });
 
+/**
+ * Reasd a the input.txt file and returns it line by line. Multiple calls to this functions should be made to scan the entire file.
+ * @param path - Path to the file. Optional, defaults to ./input.txt
+ * @returns return the next line of the read file or empty string on End of File - This is by design, on Algorithmic problems inputs are usually contiguous
+ */
 export function nextLine(path = pathToInput): string {
     if (!liner) liner = new lineByLine(path);
     const line = liner.next();
     return line === false ? '' : line.toString('ascii');
 }
 
+/**
+ * Prints to console the parameters
+ * @param args - any
+ */
 export function print(...args: any[]) {
     console.log(...args);
 }
